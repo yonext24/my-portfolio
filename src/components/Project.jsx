324d7ec1c3a949b6bf21ed2106e2e85fdb42a627
Yanzz <yonielkpo@gmail.com>
2023-01-09 13:49:41 -0300

    Added another project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react'
 import { LanguageContext } from '../contexts/LanguageContext'
 
-export default function Project({ data, parentSetState }) {
+export default function Project({ data, parentSetState, noGit }) {
   const handleProjectChange = () => {
-    parentSetState(data)
+    parentSetState({data, noGit})
   }
 
   const {language} = useContext(LanguageContext)
@@ -15,7 +15,7 @@ export default function Project({ data, parentSetState }) {
     group-hover:opacity-100 transition-[filter] pointer-events-none'>{language === 'es' ? 'Click para ver arriba' : 'Click to preview'}</p>
     
     <img src={data.image} draggable='false' className='aspect-video rounded-l-lg cursor-pointer group-hover:brightness-[.2]
-    z-[5] transition-[filter] dark:brightness-[.8]' />
+    z-[5] transition-[filter] dark:brightness-[.8] object-cover' />
     
     <aside className='bg-gradient-to-br from-rose2 to-blue2 absolute right-0 text-vertical text-center dark:text-white translate-x-2 z-30
   top-0 h-full w-1/5 rounded-r-lg group-hover:translate-x-full transition-transform cursor-pointer py-4 flex items-center text-md'>
--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -4,17 +4,18 @@ import Project from './Project'
 import data from '../assets/data.json'
 
 export default function ProjectSection() {
-  const [finalData, setFinalData] = useState(data.projects.shoes)
+  const [finalData, setFinalData] = useState({data: data.projects.shoes, noGit: false})
 
   return <section id='projects' className='w-full pt-6'>
     <div className='bg-gradient-to-tr from-rose to-blue w-4/5 mx-auto rounded-3xl backdrop-blur-[2px] py-12 px-6 flex flex-col'>
       <CurrentProject data={finalData} />
       <div className='flex flex-wrap gap-x-14 gap-y-8 mt-6 justify-center'>
-        <Project data={data.projects.shoes} parentSetState={setFinalData} />
-        <Project data={data.projects.pokeapi} parentSetState={setFinalData} />
-        <Project data={data.projects.comments} parentSetState={setFinalData} />
-        <Project data={data.projects.landingpage} parentSetState={setFinalData} />
-        <Project data={data.projects.cardspage} parentSetState={setFinalData} />
+        <Project data={data.projects.shoes} parentSetState={setFinalData} noGit={false} />
+        <Project data={data.projects.pokeapi} parentSetState={setFinalData} noGit={false} />
+        <Project data={data.projects.comments} parentSetState={setFinalData} noGit={false} />
+        <Project data={data.projects.landingpage} parentSetState={setFinalData} noGit={false} />
+        <Project data={data.projects.cardspage} parentSetState={setFinalData} noGit={false} />
+        <Project data={data.projects.landing_cviz} parentSetState={setFinalData} noGit={true} />
       </div>
     </div>
   </section>
